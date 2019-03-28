84c220a72ee446758b94fcf402d6a9aad8fa436b
MarianMilian <milanmarian@mail.ru>
2019-03-28 14:33:33 +0200

    First commit

--- src/com/myMineSweeper/example/PersonService.java
+++
@@ -1,7 +0,0 @@
-package com.myMineSweeper.example;
-
-public class PersonService {
-    public static void registerPerson(Person person) {
-        System.out.println("Registering person - " + person);
-    }
-}
